d4bb1aad02fb937d211ffd01e81a634b6f9b9ee1
sheva <vladislavshevchenko10@gmail.com>
2023-03-17 20:28:48 +0200

    here we go

---
+++ constants/assets.js
@@ -0,0 +1,57 @@
+import badge from '../assets/icons/badge.png';
+import eth from '../assets/icons/eth.png';
+import heart from '../assets/icons/heart.png';
+import left from '../assets/icons/left.png';
+import search from '../assets/icons/search.png';
+
+import logo from '../assets/images/logo.png';
+import nft01 from '../assets/images/nft01.png';
+import nft02 from '../assets/images/nft02.jpeg';
+import nft03 from '../assets/images/nft03.jpeg';
+import nft04 from '../assets/images/nft04.jpeg';
+import nft05 from '../assets/images/nft05.jpeg';
+import nftBlackBox from '../assets/images/nftBlackBox.jpeg';
+import nft07 from '../assets/images/nft07.jpeg';
+import nftMonkey1 from '../assets/images/nftMonkey1.png';
+import nftMonkey2 from '../assets/images/nftMonkey2.png';
+import nftMonkey3 from '../assets/images/nftMonkey3.png';
+import nftMonkey4 from '../assets/images/nftMonkey4.png';
+
+import person01 from '../assets/images/person01.png';
+import person02 from '../assets/images/person02.png';
+import person03 from '../assets/images/person03.png';
+import person04 from '../assets/images/person04.png';
+import sheva from '../assets/images/sheva.png';
+import messi from '../assets/images/messi.png';
+import jordanBelfort from '../assets/images/jordanBelfort.png'
+import reggie from '../assets/images/reggie.png'
+
+export default {
+   badge,
+   eth,
+   heart,
+   left,
+   search,
+
+   logo,
+   nft01,
+   nft02,
+   nft03,
+   nft04,
+   nft05,
+   nftBlackBox,
+   nft07,
+   nftMonkey1,
+   nftMonkey2,
+   nftMonkey3,
+   nftMonkey4,
+
+   person01,
+   person02,
+   person03,
+   person04,
+   sheva,
+   messi,
+   jordanBelfort,
+   reggie
+};
